e65e47f4d0ea88f74cd795df0f11ed54b20c7f8c
Mark Franklin <mark@gremlin.com>
2020-05-29 08:18:36 -0700

    initial commit

---
+++ src/main/java/com/example/alfidynamodb/AlfiDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.alfidynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfiDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfiDynamodbApplication.class, args);
+	}
+
+}
